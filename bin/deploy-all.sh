e44c6fb07ea3ee49c612f909994c91e55fe16afb
Hauton Tsang <hautonjt@users.noreply.github.com>
2024-11-19 16:45:32 -0500

    update deploy scripts

--- bin/deploy-all.sh
+++ bin/deploy-all.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+
+cd ..
+
 sudo sysctl -w vm.max_map_count=786432
 echo "vm.max_map_count=786432" | sudo tee /etc/sysctl.conf
 
 kubectl create namespace datapipeline
 
+sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-0
+sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-1
+sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-2
+
+kubectl apply -k nifi -n datapipeline
+
 kubectl create secret generic opensearch-certs -n datapipeline --from-file=opensearch-certs/certs
 helm install opensearch ./opensearch --namespace datapipeline
 helm install opensearch-dashboards ./opensearch-dashboards --namespace datapipeline
-
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo update
-helm install kube-state-metrics prometheus-community/kube-state-metrics --version 5.27.0 --namespace datapipeline
 
 helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka --version 31.0.0 --namespace datapipeline -f kafka/values.yaml
 kubectl rollout status statefulset kafka-controller -n datapipeline
@@ -20,8 +25,7 @@ sudo mkdir -p /var/lib/nifi/nifi-0
 sudo mkdir -p /var/lib/nifi/nifi-1
 sudo mkdir -p /var/lib/nifi/nifi-2
 
-sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-0
-sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-1
-sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-2
-
-kubectl apply -k nifi -n datapipeline
+kubectl apply -f beats/filebeat-kubernetes.yaml
+kubectl apply -f beats/metricbeat-kubernetes.yaml
+kubectl apply -f beats/packetbeat-kubernetes.yaml
+kubectl apply -f lab/metricbeat-prometheus-answer.yaml
---
+++ bin/deploy-safe.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd ..
+
+sudo sysctl -w vm.max_map_count=786432
+echo "vm.max_map_count=786432" | sudo tee /etc/sysctl.conf
+
+kubectl create namespace datapipeline
+
+sudo mkdir -p /var/lib/nifi/nifi-0
+sudo mkdir -p /var/lib/nifi/nifi-1
+sudo mkdir -p /var/lib/nifi/nifi-2
+
+sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-0
+sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-1
+sudo cp geoip/GeoLite2-City.mmdb /var/lib/nifi/nifi-2
+
+kubectl apply -k nifi -n datapipeline
+
+kubectl create secret generic opensearch-certs -n datapipeline --from-file=opensearch-certs/certs
+helm install opensearch ./opensearch --namespace datapipeline
+helm install opensearch-dashboards ./opensearch-dashboards --namespace datapipeline
+
+helm install kafka oci://registry-1.docker.io/bitnamicharts/kafka --version 31.0.0 --namespace datapipeline -f kafka/values.yaml
+kubectl rollout status statefulset kafka-controller -n datapipeline
+kubectl apply -f kafka-ui/kafka-ui-deployment.yaml -n datapipeline
